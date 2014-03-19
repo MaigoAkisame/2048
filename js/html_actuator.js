4cf3d06b71573eb41428bb73c58341a0b4aacba1
MaigoAkisame <maigoakisame@gmail.com>
2014-03-18 23:54:45 -0400

    OK

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -13,11 +13,15 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
   window.requestAnimationFrame(function () {
     self.clearContainer(self.tileContainer);
 
-    grid.cells.forEach(function (column) {
-      column.forEach(function (cell) {
-        if (cell) {
-          self.addTile(cell);
-        }
+    grid.cells.forEach(function (col) {
+      col.forEach(function (littleRow) {
+        littleRow.forEach(function (littleCol) {
+          littleCol.forEach(function (cell) {
+            if (cell) {
+              self.addTile(cell);
+            }
+          });
+        });
       });
     });
 
@@ -51,7 +55,7 @@ HTMLActuator.prototype.addTile = function (tile) {
 
   var wrapper   = document.createElement("div");
   var inner     = document.createElement("div");
-  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var position  = tile.previousPosition || { x: tile.x, y: tile.y, z: tile.z, w: tile.w };
   var positionClass = this.positionClass(position);
 
   // We can't use classlist because it somehow glitches when replacing classes
@@ -67,7 +71,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
     window.requestAnimationFrame(function () {
-      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+      classes[2] = self.positionClass({ x: tile.x, y: tile.y, z: tile.z, w: tile.w });
       self.applyClasses(wrapper, classes); // Update the position
     });
   } else if (tile.mergedFrom) {
@@ -95,12 +99,12 @@ HTMLActuator.prototype.applyClasses = function (element, classes) {
 };
 
 HTMLActuator.prototype.normalizePosition = function (position) {
-  return { x: position.x + 1, y: position.y + 1 };
+  return { x: position.x + 1, y: position.y + 1, z: position.z + 1, w: position.w + 1 };
 };
 
 HTMLActuator.prototype.positionClass = function (position) {
   position = this.normalizePosition(position);
-  return "tile-position-" + position.x + "-" + position.y;
+  return "tile-position-" + position.x + "-" + position.y + "-" + position.z + "-" + position.w;
 };
 
 HTMLActuator.prototype.updateScore = function (score) {
--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -28,14 +28,10 @@ KeyboardInputManager.prototype.listen = function () {
     39: 1, // Right
     40: 2, // Down
     37: 3, // Left
-    75: 0, // vim keybindings
-    76: 1,
-    74: 2,
-    72: 3,
-    87: 0, // W
-    68: 1, // D
-    83: 2, // S
-    65: 3  // A
+    87: 4, // W
+    68: 5, // D
+    83: 6, // S
+    65: 7  // A
   };
 
   document.addEventListener("keydown", function (event) {
